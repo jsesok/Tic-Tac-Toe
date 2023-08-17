aac8813f9dba08844491f308024d3fcebb492c48
Julija S <julija.sesok@gmail.com>
2023-08-17 14:43:54 +0200

    Change the handleClick function to display X when the square is clicked, using setValue

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function Square() {
   const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log("clicked");
+    setValue("X");
   }
+  
   return (
     <button 
       className="square"
