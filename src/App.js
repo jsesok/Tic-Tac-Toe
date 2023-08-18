a3f1651640895cc7003640c1c1caaf6084f37bbd
Julija S <julija.sesok@gmail.com>
2023-08-18 15:35:10 +0200

    Update handlePlay function

--- src/App.js
+++ src/App.js
@@ -83,7 +83,8 @@ export default function Game() {
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
-    // TBA
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
 
   return (
