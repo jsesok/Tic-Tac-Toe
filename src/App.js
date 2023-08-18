82871b5f23d59f50181f79a53a8805d5507a3f71
Julija S <julija.sesok@gmail.com>
2023-08-18 15:04:06 +0200

    Create a new top-level component Game

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -81,4 +81,17 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
